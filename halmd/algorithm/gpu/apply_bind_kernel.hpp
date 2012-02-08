cf95e8085d2593c16c4145f9c4fa6e6deb7d474a
Michael Kopp <mkopp@fluids.mpi-stuttgart.mpg.de>
2012-02-08 15:14:45 +0100

    algorithm: provide a gpu function like std::transform
    The binary functors from algorithm/transform.cuh can be used with one
    fixed parameter.  Two versions, one with first, one with second
    parameter fixed, are provided.

---
+++ halmd/algorithm/gpu/apply_bind_kernel.hpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2012  Michael Kopp
+ *
+ * This file is part of HALMD.
+ *
+ * HALMD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HALMD_ALGORITHM_GPU_APPLY_BIND_KERNEL_HPP
+#define HALMD_ALGORITHM_GPU_APPLY_BIND_KERNEL_HPP
+
+#include <cuda_wrapper/cuda_wrapper.hpp>
+
+#include <halmd/algorithm/gpu/transform.cuh>
+
+namespace halmd {
+namespace algorithm {
+namespace gpu {
+
+template <
+    typename functor
+  , typename input_type
+  , typename coalesced_input_type       = input_type
+  , typename output_type                = input_type
+  , typename coalesced_output_type      = output_type
+>
+struct apply_bind1st_wrapper
+{
+    cuda::function<void (coalesced_input_type const*, coalesced_output_type*, coalesced_input_type const, unsigned int)> apply;
+    static apply_bind1st_wrapper const kernel;
+};
+
+template <
+    typename functor
+  , typename input_type
+  , typename coalesced_input_type       = input_type
+  , typename output_type                = input_type
+  , typename coalesced_output_type      = output_type
+>
+struct apply_bind2nd_wrapper
+{
+    cuda::function<void (coalesced_input_type const*, coalesced_output_type*, coalesced_input_type const, unsigned int)> apply;
+    static apply_bind2nd_wrapper const kernel;
+};
+
+} // namespace gpu
+} // namespace algorithm
+} // namespace halmd
+
+#endif /* ! HALMD_ALGORITHM_GPU_APPLY_BIND_KERNEL_HPP */
